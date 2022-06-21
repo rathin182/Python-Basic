3d20c07795ca8dceb4cce5bb9abe626de8d8838f
Rathin Bagchi <rathinb745@gmail.com>
2022-06-21 16:55:15 +0530

    hello

---
+++ test.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your first number"))
+b = int(input("Enter Your 2nd Number"))
+c = input("*, /, -, +")
+d = "*"
+e = "/"
+f = "-"
+g = "+"
+if c == d:
+    print(a * b)
+
+
+
+
+
