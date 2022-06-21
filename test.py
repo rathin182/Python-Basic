e6dbf30e4b43112b31e5082f875dab84fc23fe94
Rathin Bagchi <rathinb745@gmail.com>
2022-06-21 18:10:14 +0530

    trynew

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-a = int(input("Enter your first number"))
-b = int(input("Enter Your 2nd Number"))
-c = input("*, /, -, +")
+a = int(input("Enter your first number: "))
+b = int(input("Enter Your 2nd Number: "))
+c = input("*, /, -, + :-\n")
 d = "*"
 e = "/"
 f = "-"
 g = "+"
 if c == d:
     print(a * b)
+elif c == e:
+    print(a / b)
 
+elif c == f:
+    print(a - b)
+
+elif c == g:
+    print(a + b)
 
 
 
